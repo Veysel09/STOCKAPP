8a22bc18870fd10db9e4897c2dc3d147888a836a
Veysel Aydin <weycell.aydin09@gmail.com>
2023-03-15 22:13:44 +0300

    localden token

--- src/service/axiosInstance.js
+++ src/service/axiosInstance.js
@@ -1,7 +1,10 @@
 import axios from "axios"
 
+ 
+const escapedToken = JSON.parse(localStorage.getItem("persist:root"))?.token;
+const token = escapedToken && JSON.parse(escapedToken);
 
-const axiosWithToken = axios.create({
+export const axiosWithToken = axios.create({
 baseURL: "https://14192.fullstack.clarusway.com/",
 headers: {Authorization: `Token ${token}`}
 })
