c24f09a66060e3eddf3dfb62a845ac2787870294
Veysel Aydin <weycell.aydin09@gmail.com>
2023-06-22 17:19:12 +0300

    barands imports

--- src/pages/Brands.jsx
+++ src/pages/Brands.jsx
@@ -1,5 +1,7 @@
-const Brands = () => {
-  return <div>Brands</div>;
-};
-
-export default Brands;
+import { Typography, Box, Grid, Alert, Button } from "@mui/material";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import BrandCard from "../components/BrandCard";
+import BrandModal from "../components/modals/BrandModal";
+import useStockCalls from "../hooks/useStockCalls";
+import { flexCenter } from "../styles/globalStyle";
