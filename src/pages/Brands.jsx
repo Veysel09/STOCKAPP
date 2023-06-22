0c4c620721203a90495c4a5a54fb83af7fdc848a
Veysel Aydin <weycell.aydin09@gmail.com>
2023-06-22 17:19:59 +0300

    const brands

--- src/pages/Brands.jsx
+++ src/pages/Brands.jsx
@@ -5,3 +5,9 @@ import BrandCard from "../components/BrandCard";
 import BrandModal from "../components/modals/BrandModal";
 import useStockCalls from "../hooks/useStockCalls";
 import { flexCenter } from "../styles/globalStyle";
+
+const Brands = () => {
+  const { getBrands } = useStockCalls();
+  const { brands, loading } = useSelector((state) => state.stock);
+  const [open, setOpen] = useState(false);
+  const [info, setInfo] = useState({});
