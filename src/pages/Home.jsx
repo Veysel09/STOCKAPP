76390625da776b5f1a01121ecafc827010f4ee61
Veysel Aydin <weycell.aydin09@gmail.com>
2023-06-24 10:27:45 +0300

    kpicards

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,8 @@ import { Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import { useEffect } from "react";
 import Charts from "../components/Charts";
+import KpiCards from "../components/KpiCards";
+import useStockCalls from "../hooks/useStockCalls";
 
 const Home = () => {
   return <div>Home</div>;
