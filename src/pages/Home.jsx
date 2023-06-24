7a7fe1fd338bd1ed013a1635464d7d165aa20af6
Veysel Aydin <weycell.aydin09@gmail.com>
2023-06-24 10:27:13 +0300

    home import

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,8 @@
+import { Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import { useEffect } from "react";
+import Charts from "../components/Charts";
+
 const Home = () => {
   return <div>Home</div>;
 };
