6364beddeb61d5c091e98a652d27d37107c6aa1a
Veysel Aydin <weycell.aydin09@gmail.com>
2023-06-26 20:34:31 +0300

    useStates products

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -2,16 +2,19 @@ import { useEffect, useState } from "react";
 import useStockCalls from "../hooks/useStockCalls";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-mport Button from "@mui/material/Button";
+import Button from "@mui/material/Button";
 import { useSelector } from "react-redux";
 import ProductModal from "../components/modals/ProductModal";
 import MultiSelect from "../components/MultiSelect";
 import ProductsTable from "../components/tables/ProductsTable";
 
-
 const Products = () => {
-   const { getProCatBrands } = useStockCalls();
+  const { getProCatBrands } = useStockCalls();
   const { products, brands } = useSelector((state) => state.stock);
+  const [open, setOpen] = useState(false);
+  const [info, setInfo] = useState({});
+  const [selectedBrands, setSelectedBrands] = useState([]);
+  const [selectedProducts, setSelectedProducts] = useState([]);
   return <div>Products</div>;
 };
 
