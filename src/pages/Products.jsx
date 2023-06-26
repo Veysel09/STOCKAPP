05a6d2a17ca6bfa7341780198918e2211bd7d1dd
Veysel Aydin <weycell.aydin09@gmail.com>
2023-06-26 20:32:57 +0300

    products imports

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -2,6 +2,12 @@ import { useEffect, useState } from "react";
 import useStockCalls from "../hooks/useStockCalls";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
+mport Button from "@mui/material/Button";
+import { useSelector } from "react-redux";
+import ProductModal from "../components/modals/ProductModal";
+import MultiSelect from "../components/MultiSelect";
+import ProductsTable from "../components/tables/ProductsTable";
+
 
 const Products = () => {
   return <div>Products</div>;
