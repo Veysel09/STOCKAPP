67682749f9f27403ab854f31fb0880065b99afc9
Veysel Aydin <weycell.aydin09@gmail.com>
2023-06-28 08:04:54 +0300

    purchases imports go on

--- src/pages/Purchases.jsx
+++ src/pages/Purchases.jsx
@@ -1,6 +1,9 @@
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import useStockCalls from "../hooks/useStockCalls";
+import MultiSelect from "../components/MultiSelect";
 
 const Purchases = () => {
   return <div>Purchases</div>;
