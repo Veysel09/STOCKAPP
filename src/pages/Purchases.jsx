76ae735b692c0cfc4de39bc7c3dc5beff1a62c2f
Veysel Aydin <weycell.aydin09@gmail.com>
2023-06-28 08:04:04 +0300

    Purchases import

--- src/pages/Purchases.jsx
+++ src/pages/Purchases.jsx
@@ -1,3 +1,7 @@
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { useEffect, useState } from "react";
+
 const Purchases = () => {
   return <div>Purchases</div>;
 };
