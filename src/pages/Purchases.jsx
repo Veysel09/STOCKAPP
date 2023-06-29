01f54e9cf8468a450e14f5b9e109a2305b4950f0
Veysel Aydin <weycell.aydin09@gmail.com>
2023-06-29 16:56:44 +0300

    useSelector

--- src/pages/Purchases.jsx
+++ src/pages/Purchases.jsx
@@ -6,6 +6,9 @@ import useStockCalls from "../hooks/useStockCalls";
 import MultiSelect from "../components/MultiSelect";
 
 const Purchases = () => {
+  const { purchases } = useSelector((state) => state.stock);
+  // const { getProCatBrands, getPurchases, getFirms } = useStockCalls();
+  const { getAllStockData } = useStockCalls();
   return <div>Purchases</div>;
 };
 
